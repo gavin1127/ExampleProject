ed8f4b34976dbc9d0c0be0b32d7c63b971b63773
Gavin Shepherd <Gavinshepherd6688@gmail.com>
2017-03-01 16:57:12 -0500

    first update

--- src/Java1.java
+++ src/Java1.java
@@ -2,4 +2,29 @@
  * Created by Gavin on 3/1/17.
  */
 public class Java1 {
+
+
+
+
+
+
+
+
+
+
+
+
+   else if(input.equals("Gavin"))
+       System.out.println("your mother is a hamster")
+
+
+
+
+
+
+
+
+
+
+
 }
